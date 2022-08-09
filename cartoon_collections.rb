467b5023fd274094fdd286cad31d117b2d9c8de2
daragmas <daragmas@gmail.com>
2022-08-09 10:12:36 -0400

    commit tree b60569f0541df9b7aafabd476eb2ebf1f0c9ee43

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each.with_index{|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteers
+  planeteers.map{|planeteer| planeteer.capitalize+("!")}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  calls.find{|call| call.length>4}? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese food_list
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_list.find{|food| cheese_types.include?(food)? food: nil}
 end
